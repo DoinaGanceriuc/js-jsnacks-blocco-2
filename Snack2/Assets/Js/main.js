202b913e3800cc5e71a6ddab47abbe4f28da7d29
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-10-25 12:19:23 +0200

    add html and js snack 2

---
+++ Snack2/Assets/Js/main.js
@@ -0,0 +1,31 @@
+/* Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà: nome, punti fatti e falli subiti.
+Nome sarà l’unica proprietà da compilare, mentre le altre saranno tutte settate a 0. */
+
+const squadreCalcio = [
+    {
+        nome: "Milan",
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: "Inter",
+        punti: 0,
+        falli: 0
+    },
+   {
+        nome: "Roma",
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: "Napoli",
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: "Fiorentina",
+        punti: 0,
+        falli: 0
+    },
+]
